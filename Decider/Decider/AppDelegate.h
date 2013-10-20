8c9ed3116128a151b7d08198a76849cca261ffa5
shilinfeng <shilinf@umich.edu>
2013-10-20 15:09:53 -0400

    init

---
+++ Decider/Decider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Decider
+//
+//  Created by Linfeng Shi on 10/20/13.
+//  Copyright (c) 2013 Linfeng Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Decider/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Decider
+//
+//  Created by Linfeng Shi on 10/20/13.
+//  Copyright (c) 2013 Linfeng Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
